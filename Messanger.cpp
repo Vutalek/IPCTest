76230eb20f74a3ce327825733f54c81e3170523d
Vutalek <mr.sergei02@mail.ru>
2024-08-13 13:53:19 +0300

    Messanger update

--- Messanger.cpp
+++ Messanger.cpp
@@ -7,12 +7,16 @@ typedef enum
 {
     SERVER,
     CLIENT
-} ENTITY
+}; ENTITY
 
 struct client_id
 {
-public:
-    client_id(long id1, long id2) : c_id1(id1), c_id2(id2)
+    client_id(
+              long id1,
+              long id2
+              )
+              : c_id1(id1)
+              , c_id2(id2)
     {
     }
     long c_id1;
@@ -21,8 +25,14 @@ public:
 
 struct message
 {
-public:
-    message(long t, client_id c, char* d) : type(t), client(c), data(d)
+    message(
+            long t,
+            client_id c,
+            char* d
+            )
+            : type(t)
+            , client(c)
+            , data(d)
     {
     }
 
@@ -39,16 +49,32 @@ public:
 class Connector
 {
 public:
-    void virtual open() = 0;
-    void virtual close() = 0;
+    Connector(
+              ENTITY ent,
+              std::string s_name,
+              client_id cl,
+              size_t m_size,
+              message* m
+              )
+              : entity(ent)
+              , server_name(s_name)
+              , client(cl)
+              , message_size(m_size)
+              , msg(m)
+    {
+    }
+
+    virtual void open() = 0;
+    virtual void close() = 0;
+
     ENTITY get_entity() {return entity;}
-    string get_server_name() {return server_name;}
+    std::string get_server_name() {return server_name;}
     client_id get_client() {return client;}
     size_t get_message_size() {return message_size;}
     message* get_message() {return msg;}
-private:
+protected:
     ENTITY entity;
-    string server_name;
+    std::string server_name;
     client_id client;
     size_t message_size;
     message* msg;
@@ -60,12 +86,12 @@ public:
     Messanger(Connector con) : connector(con)
     {
     }
-    void* virtual read_get_link() = 0;
-    void virtual read_release_link() = 0;
+    virtual void* read_get_link() = 0;
+    virtual void read_release_link() = 0;
 
-    void* virtual write_get_link(client_id client) = 0;
-    void virtual write_release_link() = 0;
-private:
+    virtual void* write_get_link(client_id client) = 0;
+    virtual void write_release_link() = 0;
+protected:
     Connector connector;
 };
 
